429ce36af791de5a8bdf23fc4fdd34c103c94784
amir <freqwong@gmail.com>
2025-12-07 18:37:11 +0330

    prisma 7 configuration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 // src/prisma/prisma.service.ts
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const adapter = new PrismaMariaDb({
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      connectionLimit: 5,
+    });
+    super({ adapter });
+  }
   async onModuleInit() {
     await this.$connect();
   }
