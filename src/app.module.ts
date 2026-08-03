c07d6e13670508b47e9c26a26167492e11350833
iman <iman.kh7798@gmail.com>
2026-08-03 16:45:20 +0330

    feat(images): edit images routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,8 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
+      rootPath: join(__dirname, '..', '..', 'uploads'),
+      serveRoot: '/uploads',
     }),
     BusinessModule,
     CategoriesModule,
